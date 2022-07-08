528ee135446496ca6b6e44bfb72573403f65f47e
4Shivaraj <4shivaraj.gowda@gmail.com>
2022-07-09 03:14:03 +0530

    [Shivaraj] Added Learning Problem Of Reflection

--- ReflectionLearningProblem/ReflectionLearningProblem/Program.cs
+++ ReflectionLearningProblem/ReflectionLearningProblem/Program.cs
@@ -1,2 +1,36 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Welcome To Reflection Learning Problem");
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReflectionLearningProblem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome To Reflection Learning Problem");
+            string assemblypath = @"C:\Users\4shiv\OneDrive\Desktop\Fellowship\Assignments\Assignment_Day_9\NewAddressBook\AddressBookProblem\AddressBookProblem\bin\Debug\net6.0\AddressBookProblem.dll";
+            ReflectionFactory.TestAssemblyUsingReflection(assemblypath);
+            Console.ReadLine();
+        }
+        public class ReflectionFactory
+        {
+            public static void TestAssemblyUsingReflection(string assemblypath)
+            {
+                Assembly assembly = Assembly.LoadFrom(assemblypath);
+                Type[] types = assembly.GetTypes();
+                if (assembly != null)
+                {
+                    foreach (Type type in types)
+                    {
+                        Console.WriteLine(type.FullName);
+                    }
+                }
+            }
+        }
+    }
+}
+
